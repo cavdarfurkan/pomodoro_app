a475be64899e68eeeb74e79b15d77d793dfaaae0
cavdarfurkan <m.furkancavdar@gmail.com>
2022-09-11 20:25:36 +0300

    pageview and build widgets from the list

--- lib/view/timer_view.dart
+++ lib/view/timer_view.dart
@@ -3,55 +3,50 @@ import 'package:provider/provider.dart';
 import 'package:pie_timer/pie_timer.dart';
 
 import '../view_model/timer_view_model.dart';
+import 'package:pomodoro_app/temp/app_scroll_behavior.dart'; //Temp
 
 class TimerView extends StatelessWidget {
   const TimerView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Consumer<TimerViewModel>(
-          builder: (context, model, child) => Column(
-            children: [
-              Visibility(
-                visible: model.vis1,
-                maintainState: false,
-                child: _buildPieWidget(model),
-              ),
-              Visibility(
-                visible: model.vis2,
-                maintainState: false,
-                child: _buildPieWidget(model),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () => model.changeVisibility(),
-                      child: const Text('Next'),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ],
+    final pageController = PageController(initialPage: 0);
+    return Consumer<TimerViewModel>(
+      builder: (context, model, child) => PageView(
+        controller: pageController,
+        scrollDirection: Axis.horizontal,
+        scrollBehavior: AppScrollBehaviour(),
+        onPageChanged: (value) => debugPrint('$value'),
+        children: [
+          for (var pies in _pieWidget(model)) pies,
+        ],
+      ),
     );
   }
 }
 
-PieTimer _buildPieWidget(TimerViewModel model) {
-  return PieTimer(
-    duration: model.timerModel.duration,
-    radius: 150.0,
-    pieColor: model.timerModel.pieColor,
-    fillColor: model.timerModel.fillColor,
-    borderWidth: 10.0,
-    borderColor: Colors.white,
-    shadowElevation: 10.0,
-    enableTouchControls: true,
-  );
+List<Widget> _pieWidget(TimerViewModel model) {
+  List<Widget> listPieTimer = List.empty(growable: true);
+
+  for (var m in model.listTimerModel) {
+    listPieTimer.add(Center(
+      child: Column(
+        children: [
+          PieTimer(
+            duration: m.duration,
+            radius: 150.0,
+            pieColor: m.pieColor,
+            fillColor: m.fillColor,
+            borderWidth: 10.0,
+            borderColor: Colors.white,
+            shadowElevation: 5.0,
+            enableTouchControls: true,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("qwe")),
+        ],
+      ),
+    ));
+  }
+
+  return listPieTimer;
 }
--- lib/view_model/timer_view_model.dart
+++ lib/view_model/timer_view_model.dart
@@ -1,76 +1,47 @@
 import 'package:flutter/material.dart';
 import '../model/timer_model.dart';
 
-enum PomodoroType { main, go, shortBreak, longBreak, pause }
-
 class TimerViewModel extends ChangeNotifier {
-  late PomodoroType type = PomodoroType.main;
-  late TimerModel timerModel;
-  bool vis1 = true;
-  bool vis2 = false;
+  late List<TimerModel> listTimerModel = [
+    mainModel,
+    goModel,
+    shortBreakModel,
+    longBreakModel,
+  ];
+
+  late TimerModel mainModel;
+  late TimerModel goModel;
+  late TimerModel shortBreakModel;
+  late TimerModel longBreakModel;
 
   TimerViewModel() {
     _buildModel();
   }
 
   void _buildModel() {
-    if (type == PomodoroType.main) {
-      timerModel = const TimerModel(
-        duration: Duration(seconds: 10),
-        radius: 100.0,
-        pieColor: Colors.blueAccent,
-        fillColor: Colors.red,
-      );
-    }
-    if (type == PomodoroType.go) {
-      timerModel = const TimerModel(
-        duration: Duration(minutes: 25),
-        radius: 100.0,
-        pieColor: Colors.blueAccent,
-        fillColor: Colors.yellow,
-      );
-    }
-    if (type == PomodoroType.shortBreak) {
-      timerModel = const TimerModel(
-        duration: Duration(minutes: 5),
-        radius: 100.0,
-        pieColor: Colors.blueAccent,
-        fillColor: Colors.white,
-      );
-    }
-    if (type == PomodoroType.longBreak) {
-      timerModel = const TimerModel(
-        duration: Duration(minutes: 15),
-        radius: 100.0,
-        pieColor: Colors.blueAccent,
-        fillColor: Colors.green,
-      );
-    }
-    if (type == PomodoroType.pause) {
-      timerModel = TimerModel(
-        duration: const Duration(seconds: 50),
-        radius: 100.0,
-        pieColor: Colors.grey[400]!,
-        fillColor: Colors.grey,
-      );
-    }
+    mainModel = const TimerModel(
+      duration: Duration(seconds: 10),
+      radius: 100.0,
+      pieColor: Colors.blueAccent,
+      fillColor: Colors.red,
+    );
+    goModel = const TimerModel(
+      duration: Duration(minutes: 25),
+      radius: 100.0,
+      pieColor: Colors.blueAccent,
+      fillColor: Colors.yellow,
+    );
+    shortBreakModel = const TimerModel(
+      duration: Duration(minutes: 5),
+      radius: 100.0,
+      pieColor: Colors.blueAccent,
+      fillColor: Colors.white,
+    );
+    longBreakModel = const TimerModel(
+      duration: Duration(minutes: 15),
+      radius: 100.0,
+      pieColor: Colors.blueAccent,
+      fillColor: Colors.green,
+    );
   }
-
-  void changeVisibility() {
-    vis1 = !vis1;
-    vis2 = !vis2;
-    nextModel();
-  }
-
-  void nextModel() {
-    int i = type.index;
-    int n = i + 1;
-    if (i == PomodoroType.values.length - 1) {
-      n = 0;
-    }
-    type = PomodoroType.values.elementAt(n);
-    _buildModel();
-    notifyListeners();
-  }
-
 }
